ff12c4e22207cdc0b16a27ff1ee6e140635402ff
BSKOM <b.shelestua@gmail.com>
2021-03-01 09:15:12 +0200

    init: start typical array problem v 1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
+  if (array !== undefined && array.length !== 0) return Math.min.apply(null, array)
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array !== undefined && array.length !== 0) return Math.max.apply(null, array)
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array !== undefined && array.length !== 0) return array.reduce((a, b) => a + b) / array.length
   return 0;
 }
